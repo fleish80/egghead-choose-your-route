fa27fd240118395d6e71ff8e4bc373de20746139
fleish80 <Fleish.D@gmail.com>
2018-12-07 16:23:25 +0200

    Lesson 10

---
+++ src/app/people/person-resolver.service.ts
@@ -0,0 +1,18 @@
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { PeopleService } from './people.service';
+
+@Injectable()
+export class PersonResolverService implements Resolve<any> {
+
+  constructor(private peopleService: PeopleService) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const personId = +route.params['personId'];
+    return this.peopleService.getPersonById(personId);
+  }
+}
